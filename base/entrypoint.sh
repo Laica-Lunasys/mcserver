2a95a9fd578a4757289503ce0a15030e9b844fc3
Laica Lunasys <laica.lunasys@gmail.com>
2021-02-08 19:22:25 +0900

    Fix replacement

--- base/entrypoint.sh
+++ base/entrypoint.sh
@@ -2,11 +2,13 @@
 set -e
 cd "$(dirname "$0")"
 
-sed -i \
-    -e 's/!!SERVER_NAME!!/'"$SERVER_NAME"'/g' \
-    -e 's/!!SERVER_ID!!/'"$SERVER_ID"'/g' \
-    -e 's/!!WHITE_LIST!!/'"$WHITE_LIST"'/g' \
-    -e 's/!!ONLINE_MODE!!/'"$ONLINE_MODE"'/g' \
-    /app/server.properties
+if [ -e /app/server.properties ]; then
+    sed -i \
+        -e 's/!!SERVER_NAME!!/'"$SERVER_NAME"'/g' \
+        -e 's/!!SERVER_ID!!/'"$SERVER_ID"'/g' \
+        -e 's/!!WHITE_LIST!!/'"$WHITE_LIST"'/g' \
+        -e 's/!!ONLINE_MODE!!/'"$ONLINE_MODE"'/g' \
+        /app/server.properties
+fi
 
 exec java -jar /paper/paper.jar -nogui
